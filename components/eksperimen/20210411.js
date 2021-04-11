e30b4655ffcf40a6a2b2a4c0f6a7156d82accbb5
sakitkepala <andikapriyotama@gmail.com>
2021-04-11 15:53:03 +0700

    Load screen eksperimen secara dinamis/lazy & code split

---
+++ components/eksperimen/20210411.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+function Screen() {
+  return (
+    <div className="eksperimen-display">
+      <h1>Eksperimen 1</h1>
+
+      <p>Eksperimen interaktif...</p>
+    </div>
+  );
+}
+
+export { Screen };
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,55 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import dynamic from "next/dynamic";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+/**
+ * Untuk ditampilkan di halaman depan, butuh:
+ * 1. data/metadata tiap eksperimen
+ * 2. componen screen/aplikasi eksperimen individualnya
+ */
+// TODO: pindah jadi import dari modul yang ngehandle logic untuk eksperimen
+// * pakai hook, mungkin?
+const dataEksperimen = {
+  tanggal: "2021-04-11",
+  judul: "Spike",
+  deskripsi: "Bla bla bla. Awesome, ye kan?",
+};
+
+const {
+  judul: judulEksperimen,
+  deskripsi: deskripsiEksperimen,
+} = dataEksperimen;
+
+export default function Depan() {
+  const ScreenEksperimen = dynamic(
+    () => import("../components/eksperimen/20210411").then((mod) => mod.Screen),
+    {
+      ssr: false,
+      loading: () => (
+        <>
+          <p>
+            Kamu sedang berada di eksperimen web interaktif halaman depan
+            sakitkepala.dev
+          </p>
+          <p>Sedang memuat eksperimen...</p>
+        </>
+      ),
+    }
+  );
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>
+          {judulEksperimen} - Sebuah eksperimen web interaktif | Di halaman
+          depan sakitkepala.dev
+        </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ScreenEksperimen />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
