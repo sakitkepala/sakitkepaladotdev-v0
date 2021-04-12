44502bff28efee47c5827e6eb3119fa2bea53ff5
sakitkepala <andikapriyotama@gmail.com>
2021-04-12 19:30:15 +0700

    Page tulisan individual dari file MDX di direktori khusus

---
+++ pages/tulisan/t/[slug].js
@@ -0,0 +1,37 @@
+import hydrate from "next-mdx-remote/hydrate";
+import {
+  getListInfoPostingan,
+  getPostinganMenurutSlug,
+} from "../../../lib/api/tulisan";
+
+export async function getStaticPaths() {
+  const paths = getListInfoPostingan().map((postingan) => ({
+    params: { slug: postingan.slug },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postingan = await getPostinganMenurutSlug(params.slug);
+
+  return {
+    props: {
+      postingan,
+    },
+  };
+}
+
+export default function TulisanPage({ postingan }) {
+  const { sumber, info } = postingan;
+  const konten = hydrate(sumber);
+  return (
+    <div className="lembar-tulisan">
+      <h1>{info.judul}</h1>
+      {konten}
+    </div>
+  );
+}
