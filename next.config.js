28cd745ddcb42e24deee10de9423a8fb3ebd25be
sakitkepala <andikapriyotama@gmail.com>
2021-05-04 20:25:09 +0700

    Set up script deploy gh-pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-// const isProd = process.env.NODE_ENV === "production";
+const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  // Use the CDN in production and localhost for development.
-  // assetPrefix: isProd ? "https://dev.sakitkepala.dev" : "",
-  assetPrefix: "",
+  assetPrefix: !debug ? "/sakitkepaladotdev/" : "",
 };
