88472aadd81560ce6fc84331f92d7fb97604590d
sakitkepala <andikapriyotama@gmail.com>
2021-05-09 17:40:42 +0700

    Layout halaman lab layar 600px

--- pages/lab.js
+++ pages/lab.js
@@ -1,13 +1,15 @@
 import Head from "next/head";
 import { motion } from "framer-motion";
+import { FiExternalLink } from "react-icons/fi";
 import { getSemuaSortir } from "../lib/api/eksperimen";
 import { LogoHeader } from "../components/logo-header";
 import { MenuNavigasi } from "../components/navigasi";
 import { NavLink } from "../components/nav-link";
 
-import name from "classnames";
+import classnames from "classnames";
 import old from "../styles/Dika.module.scss";
 import layout from "../styles/Layout.module.scss";
+import lab from "../styles/Lab.module.scss";
 
 function ListItemEksperimen({ info }) {
   const { repo, tanggal, judul, source } = info;
@@ -39,97 +41,77 @@ export default function HalamanLab({ listInfo }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className={old.halaman}>
+      <div className={classnames(layout.halaman, lab.antiKoleps)}>
         <header className={layout.header}>
           <LogoHeader />
           <MenuNavigasi />
         </header>
 
-        <motion.main {...propsMotionTransisi}>
-          <div className={old["bagian-lab"]}>
-            <div
-              className={name(
-                old["bagian-lab__deskripsi"],
-                old["bagian-lab__deskripsi--eksperimen"]
-              )}
-            >
-              <h1
-                className={old["bagian-lab__heading-deskripsi"]}
-                style={{ color: "lightsteelblue" }}
-              >
-                Eksperimen
-              </h1>
-
-              <p className={old["bagian-lab__tulisan-deskripsi"]}>
-                {/* TODO: */}
-                <em style={{ color: "lightsteelblue" }}>...to be developed</em>
-              </p>
-            </div>
-
-            <ul className={old["bagian-lab__list-eksperimen"]}>
-              <li>
-                {/* TODO: */}
-                <article className={old["lab-artikel"]}>
-                  {/* <NavLink href="/dika"> */}
-                  <div
-                    className={old["lab-artikel__thumb"]}
-                    style={{
-                      borderColor: "lightsteelblue",
-                      backgroundColor: "transparent",
-                    }}
-                  >
-                    <img />
-                  </div>
-                  {/* </NavLink> */}
-
-                  {/* <NavLink href="/dika"> */}
-                  <h2
-                    className={old["lab-artikel__judul"]}
-                    style={{ color: "lightsteelblue" }}
-                  >
-                    ...to be developed
-                  </h2>
-                  {/* </NavLink> */}
-                </article>
-              </li>
-            </ul>
-
-            <div
-              className={name(
-                old["bagian-lab__deskripsi"],
-                old["bagian-lab__deskripsi--projek"]
-              )}
-            >
-              <h1 className={old["bagian-lab__heading-deskripsi"]}>Projek</h1>
-
-              <p className={old["bagian-lab__tulisan-deskripsi"]}>
+        <motion.main className={lab.main} {...propsMotionTransisi}>
+          <div className={lab.bagianLab}>
+            <div>
+              <h1 className={lab.teksHeadingBagian}>Projek</h1>
+              <p className={lab.teksDeskripsiBagian}>
                 Projek-projek menarik yang mau kucoba.
               </p>
             </div>
 
-            <ul className={old["bagian-lab__list-projek"]}>
-              <li>
-                <article className={old["lab-artikel"]}>
-                  <NavLink href="https://www.github.com/sakitkepala/wkwkbudgetapp">
-                    <div
-                      className={old["lab-artikel__thumb"]}
-                      style={{
-                        backgroundImage: "url(/wkwkbudgetapp.jpg)",
-                        backgroundSize: "cover",
-                        backgroundPositionX: "center",
-                        backgroundPositionY: "65%",
-                      }}
-                    />
-                  </NavLink>
+            <div className={lab.listArtikel}>
+              <article
+                className={classnames(lab.itemArtikel, lab.itemArtikelPertama)}
+              >
+                <NavLink href="https://www.github.com/sakitkepala/wkwkbudgetapp">
+                  <div
+                    className={lab.imageArtikel}
+                    // TODO: nanti digenerate otomatis dari data list
+                    style={{
+                      backgroundImage: "url(/wkwkbudgetapp.jpg)",
+                      backgroundSize: "cover",
+                      backgroundPositionX: "center",
+                      backgroundPositionY: "65%",
+                    }}
+                  />
+                </NavLink>
 
-                  <h2 className={old["lab-artikel__judul"]}>
-                    <NavLink href="https://www.github.com/sakitkepala/wkwkbudgetapp">
-                      Budgeting App &#40;WIP&#41;
-                    </NavLink>
-                  </h2>
-                </article>
-              </li>
-            </ul>
+                <h2 className={lab.judulArtikel}>
+                  <NavLink href="https://www.github.com/sakitkepala/wkwkbudgetapp">
+                    Budgeting App &#40;WIP&#41; <FiExternalLink size="18" />
+                  </NavLink>
+                </h2>
+              </article>
+            </div>
+          </div>
+
+          <div className={classnames(lab.bagianLab, lab.bagianEksperimen)}>
+            <div style={{ color: "lightsteelblue" }}>
+              <h1 className={lab.teksHeadingBagian}>Eksperimen</h1>
+              <p className={lab.teksDeskripsiBagian}>
+                <em>...to be developed</em>
+              </p>
+            </div>
+
+            <div className={lab.listArtikel}>
+              <article
+                className={classnames(lab.itemArtikel, lab.itemArtikelPertama)}
+              >
+                <div
+                  className={lab.imageArtikel}
+                  style={{
+                    borderColor: "lightsteelblue",
+                    backgroundColor: "transparent",
+                  }}
+                >
+                  <img />
+                </div>
+
+                <h2
+                  className={lab.judulArtikel}
+                  style={{ color: "lightsteelblue" }}
+                >
+                  <em>...coming soon!</em>
+                </h2>
+              </article>
+            </div>
           </div>
         </motion.main>
 
